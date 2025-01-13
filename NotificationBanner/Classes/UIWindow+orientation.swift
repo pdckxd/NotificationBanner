f0881d86d6f455630cd7410132c3c5da6bbfcb41
Alex Chen <pdckxd@gmail.com>
2025-01-13 22:31:17 +0800

    Update UIWindow+orientation.swift

--- NotificationBanner/Classes/UIWindow+orientation.swift
+++ NotificationBanner/Classes/UIWindow+orientation.swift
@@ -10,18 +10,23 @@ import UIKit
 extension UIWindow {
 
     public var width: CGFloat {
-        let orientation = UIDevice.current.orientation
-        switch orientation {
-        case .landscapeLeft, .landscapeRight:
-            return max(frame.width, frame.height)
-        case .portrait, .portraitUpsideDown:
-            return min(frame.width, frame.height)
-        default:
+        #if !os(tvOS)
+            let orientation = UIDevice.current.orientation
+            switch orientation {
+            case .landscapeLeft, .landscapeRight:
+                return max(frame.width, frame.height)
+            case .portrait, .portraitUpsideDown:
+                return min(frame.width, frame.height)
+            default:
+                return frame.width
+            }
+        #else
             return frame.width
-        }
+        #endif
     }
 
     public var height: CGFloat {
+        #if !os(tvOS)
         let orientation = UIDevice.current.orientation
         switch orientation {
         case .landscapeLeft, .landscapeRight:
@@ -31,6 +36,9 @@ extension UIWindow {
         default:
             return frame.height
         }
+        #else
+            return frame.height
+        #endif
     }
 
 }
